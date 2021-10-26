3588f73325031a83f26633375d615913718fa379
Iliya <n01427009@unf.edu>
2021-10-26 15:02:54 -0400

    reworked constants file

--- controllers/yamor_qlearning/tools/constants.py
+++ controllers/yamor_qlearning/tools/constants.py
@@ -1,45 +1,31 @@
 from datetime import date
+import math
 
-
-# Constants
+# NN params
+# NN_TYPE = "FCNN"
+NN_TYPE = "CNN"
 EPSILON = 1
 GAMMA = 0.1
 MIN_EPSILON = 0.1
 T = 0.1
-BATCH_SIZE = 32
-# BATCH_SIZE = 10
+
+# Episode params
+EPISODE = 0  # starting Episode number
 # MAX_EPISODE = 20000
 MAX_EPISODE = 7000
 # MAX_EPISODE = 5000
-# EPISODE_LIMIT = 5010   # Limiting Replay Memory to 200 Episodes
-EPISODE_LIMIT = 6   # Limiting Replay Memory to 200 Episodes
-BUFFER_LIMIT = 200
-
-# TODO: make capacity 200k
-# MEMORY_CAPACITY = 10000  # this equals to a memory pool of over 4 mil spots, and with 5000 episodes each having 577
-MEMORY_CAPACITY = 2**22  # this equals to a memory pool of over 4 mil spots, and with 5000 episodes each having 577
-#                       of data we would only need just over 3 mil spots, so this should work and optimize things a bit
 UPDATE_PERIOD = 10
-EPISODE = 0
+
+# Batch params
+BATCH_SIZE = 32
+BATCH_PERCENT = 0.1
+BUFFER_LIMIT = int(MAX_EPISODE*BATCH_PERCENT) \
+    if math.modf(float(MAX_EPISODE*BATCH_PERCENT))[0] == 0\
+    else int(math.modf(float(MAX_EPISODE*BATCH_PERCENT))[1])
+
+# Time params
 DATE_TODAY = date.today()
 
-TOTAL_ELAPSED_TIME = 0
+# Robot params
 LEADER_ID = 1
-TRIAL_PATH = ""
-TRIAL_NAME = ""
-REPLAY_MEMORY_EPISODE = 1
-
-
-BASE_LOGS_FOLDER = None
-# NN_TYPE = None
-# POLICY_NET = None
-# TARGET_NET = None
-
 NUM_MODULES = 3
-INITIAL = [0, 0.5, 0]
-
-RE_ADJUST = False
-
-
-
-
