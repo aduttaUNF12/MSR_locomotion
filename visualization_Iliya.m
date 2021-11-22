eb911851387de6d6954dc3e58ba4a2f11fb4badf
Ayan Dutta <31738725+aduttaUNF12@users.noreply.github.com>
2021-11-22 11:37:18 -0500

    Add files via upload
    1. Adds support for moving mean of last k episodes.
    2. plots Exploration vs. Exploitation chart.

--- visualization_Iliya.m
+++ visualization_Iliya.m
@@ -1,4 +1,4 @@
-FILENAME = 'C:\Users\n01388138\Downloads\5000_first_vector_implementation_test.txt';
+FILENAME = 'C:\Users\n01388138\Downloads\CNN_actoins_test1.txt';
 [rwd, loss] = dataImport_iliya(FILENAME);
 %% average reward per episode
 %rwd = MODULES1100Ep.r;
@@ -9,13 +9,32 @@ rwd = cumsum(rwd);
 idx = 1:numel(rwd);
 idx = idx';
 avgr = rwd ./ idx;
+M = movmean(avgr,[100 0]);%moving mean of last 100 epiosdes
 plot(avgr)
+hold all
+plot(M)
+hold off
 xlabel('Episodes','FontSize',14);
 ylabel('Average Reward','FontSize',14);
 xlim([-100 5100])
 filename = strcat('avg_rwd.png');
-saveas(gcf,filename);
+%saveas(gcf,filename);
 % 
+
+%% exploration vs. exploitation
+eps = run1.eps * 100;
+exp = 100 - eps;
+e = [1:4999];
+yyaxis left
+plot(e,eps);
+ylabel('Exploration')
+
+yyaxis right
+plot(e,exp);
+ylabel('Exploitation');
+xlabel('episodes');
+title('Exploration vs. Exploitation');
+
 
 %% loss per episode
 figure();
