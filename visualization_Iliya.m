a13dc39310e3f45ad9efabc4109314bc89ef359a
Ayan Dutta <31738725+aduttaUNF12@users.noreply.github.com>
2022-02-21 12:40:01 -0500

    Add files via upload
    plots raw reward values along with a moving mean in the same figure.

--- visualization_Iliya.m
+++ visualization_Iliya.m
@@ -1,4 +1,4 @@
-FILENAME = 'C:\Users\n01388138\Downloads\CNN_actoins_test1.txt';
+FILENAME = 'C:\Users\n01388138\Downloads\FCNN_3mod_run5_200dec.txt';
 [rwd, loss] = dataImport_iliya(FILENAME);
 %% average reward per episode
 %rwd = MODULES1100Ep.r;
@@ -9,7 +9,7 @@ rwd = cumsum(rwd);
 idx = 1:numel(rwd);
 idx = idx';
 avgr = rwd ./ idx;
-M = movmean(avgr,[100 0]);%moving mean of last 100 epiosdes
+M = movmean(rwd,[100 0]);%moving mean of last 100 epiosdes
 plot(avgr)
 hold all
 plot(M)
@@ -23,27 +23,36 @@ filename = strcat('avg_rwd.png');
 
 %% reward plot (moving mean)
 [rwd, loss] = dataImport_iliya(FILENAME);
-M = movmean(rwd,[100 0]);%moving mean of last 100 epiosdes
-plot(M)
+episode_count = 400;
+rwd= rwd(1:episode_count,1);
+X=1:episode_count;
+%scatter(X,rwd);
+plot(rwd,'Linewidth', 0.1);
+hold all;
+M = movmean(rwd,[50 0]);%moving mean of last 100 epiosdes
+plot(M, 'Linewidth', 2);
+hold off;
 xlabel('Episodes','FontSize',14);
 ylabel('Reward','FontSize',14);
-legend('Moving Average (100 episodes)','FontSize',12, 'Location','best')
-xlim([-100 5100])
-filename = strcat('movmean_rwd.png');
+legend('Episodic Reward','Moving Average (50 episodes)','FontSize',12, 'Location','best')
+max_x = (numel(rwd)+100);
+xlim([-100 max_x])
+filename = strcat('movemean_rwd.png');
 saveas(gcf,filename);
 
-
 %% exploration vs. exploitation
-eps = run1.eps * 100;
+eps = expl * 100;
 exp = 100 - eps;
-e = [1:4999];
+e = [1:numel(eps)];
 yyaxis left
 plot(e,eps);
 ylabel('Exploration')
+ylim([-10 110])
 
 yyaxis right
 plot(e,exp);
 ylabel('Exploitation');
+ylim([-10 110])
 xlabel('episodes');
 title('Exploration vs. Exploitation');
 
